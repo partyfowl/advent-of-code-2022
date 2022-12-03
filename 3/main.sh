69b54ad9861a2b3ceaabaeb46f54313a5be1a8cc
partyfowl <partyfowl@users.noreply.github.com>
2022-12-03 22:20:32 +0000

    made shellcheck happy

--- 3/main.sh
+++ 3/main.sh
@@ -7,7 +7,7 @@ badge_priority_sum=0
 group_mem=0
 
 get_priority () {
-    ascii_value=$(printf %d \'$1)
+    ascii_value=$(printf %d \'"$1")
     
     if (( ascii_value > 90 ))
     then
@@ -18,7 +18,8 @@ get_priority () {
     echo $priority
 }
 
-for var in $(cat input.txt)
+# for var in $(cat input.txt | head -12)
+while IFS= read -r var
 do
     group_mem=$((group_mem+1))
 
@@ -31,17 +32,19 @@ do
 
     if ((group_mem == 3))
     then
+        # shellcheck disable=SC2086
         set -- $group
         badge=$(echo "$1" | grep -o "[$2]" | tr -d '\n' | grep -o "[$3]")
-        badge_priority=$(get_priority $badge)
+        badge_priority=$(get_priority "$badge")
         badge_priority_sum=$((badge_priority_sum+badge_priority))
         group_mem=0
     fi
 
     character=$(echo "${var:0:${#var}/2}" | grep -o "[${var:${#var}/2}]" | head -1)
-    priority=$(get_priority $character)
+    priority=$(get_priority "$character")
     priority_sum=$((priority_sum+priority))
     
-done
-echo Part 1: $priority_sum
-echo Part 2: $badge_priority_sum
+done < input.txt
+
+echo "Part 1: $priority_sum"
+echo "Part 2: $badge_priority_sum"
