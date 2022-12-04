4c16850a1308d67cdd8e06de16d1a6c414e523bd
partyfowl <partyfowl@users.noreply.github.com>
2022-12-04 00:00:10 +0000

    mathematical refactor to remove some bloat

--- 2/part2.py
+++ 2/part2.py
@@ -1,1 +1,1 @@
-print(sum([(i+j)%3+1+(j-1)*3 for i,j in [[(ord(j)-64)%23 for j in i.split(' ')] for i in open('input.txt').read().strip().split('\n')]]))
+print(sum([(i+j)%3+j*3-2 for i,j in [[(ord(j)-64)% 23 for j in i.split(' ')] for i in open('input.txt').read().strip().split('\n')]]))
